89ad010be61474779e586fd54ca26dc103fcaa6c
Runa Barik <runabk@ifi.uio.no>
2019-02-28 11:13:33 +0100

    Parallel tracebox of various transport protocols

--- src/tracebox/tracebox.cc
+++ src/tracebox/tracebox.cc
@@ -872,7 +872,7 @@ int doTracebox(std::shared_ptr<Packet> pkt_shrd,uint8_t dscp, tracebox_cb_t *cal
 		if (isPcap(iface))
 			rcv = PcapSendRecv(pkt, iface);
 		else{ // Write both pkt & rcv to pcap file
-			rcv = pkt->SendRecv(iface, tbx_default_timeout, 1.0);
+			rcv = pkt->SendRecv(iface, tbx_default_timeout, 1);
 			if(!isPcap(iface))
 				writePcap(pkt);
 		}
